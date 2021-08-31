3c5715754018a7e41c4164242889a45fdc818866
rohithtaddi4 <81646929+rohithtaddi4@users.noreply.github.com>
2021-08-31 22:02:28 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ class Home extends React.Component {
 componentDidMount () {
   sessionStorage.clear('Location');{/* */}
   axios({
-    url: 'https://limitless-shore-17684.herokuapp.com/locations',
+    url: 'https://warm-atoll-35409.herokuapp.com/locations',
     method: "GET",
     headers: { 'Content-Type': 'application/json' }
   }).then(response =>{
@@ -23,7 +23,7 @@ componentDidMount () {
   }).catch()
 
   axios({
-    url: 'https://limitless-shore-17684.herokuapp.com/mealtypes',
+    url: 'https://warm-atoll-35409.herokuapp.com/mealtypes',
     method: "GET",
     headers: { 'Content-Type': 'application/json' }
   }).then(response => {
